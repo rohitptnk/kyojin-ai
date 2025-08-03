b797d1daf39ad98526db562922ae3451f7103d68
Rohit Patnaik <rohitptnk03@gmail.com>
2025-08-04 00:33:59 +0530

    Add about section

--- app.py
+++ app.py
@@ -46,6 +46,11 @@ async def read_index(request: Request):
 async def read_docchat(request: Request):
     return templates.TemplateResponse("docchat.html", {"request": request})
 
+@app.get("/about", response_class=HTMLResponse)
+async def read_about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+
 @app.post("/upload/")
 @limiter.limit("5/day")
 async def upload(request: Request, files: List[UploadFile] = File(...)):
